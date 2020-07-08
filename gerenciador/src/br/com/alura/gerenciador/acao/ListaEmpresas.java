8beac0ee5c730ad5b289716fcd294b95742571eb
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2020-07-07 23:56:29 -0300

    Aula 04 - HttpSession - Atividade 06 - Autorizando o acesso
    Implementação da autorização de acesso a todas as ações, exceto às ações de login (Login e MostraFormLogin) verificando se o usuário se autenticou, ou seja, fez login ou não.

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -1,13 +1,12 @@
 package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.model.Banco;
 import br.com.alura.gerenciador.model.Empresa;
@@ -19,7 +18,7 @@ import br.com.alura.gerenciador.model.Empresa;
  */
 public class ListaEmpresas implements Acao {
 	
-	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		
 		System.out.println("Listando empresa");
 		
--- gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
@@ -16,9 +17,19 @@ public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
-		String paramAcao = request.getParameter("acao");		
+		String paramAcao = request.getParameter("acao");
+		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoLogado = sessao.getAttribute("usuarioLogado") == null;
+		boolean acaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("MostraFormLogin"));
+		
+		if(acaoProtegida && usuarioNaoLogado) {
+			response.sendRedirect("entrada?acao=MostraFormLogin");
+			return;
+		}
+		
+				
 		String nomeClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 		
 		String nome;
