e7782bc3a7089f753bdc68485e6ab53e39ecd7f6
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2021-06-21 23:11:53 -0300

    Ajustes finais
    Inclusão de comentários e também implementação do método removerTodosItens() da classe Carrinho.

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -3,19 +3,28 @@ package br.com.casadocodigo.loja.controllers;
 import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.DadosPagamento;
 
+/**
+ * A classe RestTemplate é a classe responsável por fazer integrações entre sistemas, ou seja, enviar requisições de uma aplicação 
+ * para outra aplicação independente do navegador.  
+ * @author Rodrigo
+ *
+ */
 @RequestMapping("/pagamento")
 @Controller
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
 public class PagamentoController {
 	
 	@Autowired
@@ -24,6 +33,16 @@ public class PagamentoController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	/**
+	 * Uso da classe Callable para fazer chamadas assíncronas para finalizar pagamentos. Desta forma somente o usuário que 
+	 * requisitou a finalização do pagamento aguardará a resposta do processamento. Os demais usuários continuarão usando a 
+	 * apliação sem nenhum problema ou espera de finalização do pagamento. 
+	 * Observação: Foi usado um dos recursos do Java 8. Esta forma de usar lambda nos permite criar um objeto do mesmo tipo esperado 
+	 * pelo retorno do método, evitando que criemos uma classe anônima. 
+	 * Neste caso é perfeitamente aplicável o recurso, porque na interface Callable só há um método, de nome call.
+	 * @param redirectAttributes
+	 * @return
+	 */
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
 	public Callable<ModelAndView> finalizar(RedirectAttributes redirectAttributes) {
 		
@@ -32,18 +51,19 @@ public class PagamentoController {
 			String uri = "http://book-payment.herokuapp.com/payment";
 			
 			try {
+				// Parâmetros do método postForObject: O primeiro deles é a url na qual queremos enviar a requisição, 
+				// o segundo é o objeto que representa a mensagem (o corpo) da requisição 
+				// e por último uma classe na qual esperamos receber uma resposta do tipo.
 				String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);		
 				System.out.println(response);	
-				redirectAttributes.addFlashAttribute("sucesso", response);		
-				return new ModelAndView("redirect:/produtos");
+				redirectAttributes.addFlashAttribute("sucesso", response);
+				carrinho.removerTodosItens();
 			} catch (HttpClientErrorException e) {
 				//e.printStackTrace();
 				redirectAttributes.addFlashAttribute("falha", "Valor maior que o permitido");
-				return new ModelAndView("redirect:/produtos");			
-			}			
-		};
-		
+			}
+			
+			return new ModelAndView("redirect:/produtos");
+		};		
 	}
-	
-
 }
--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -11,6 +11,15 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
+/**
+ * 
+ * @author Rodrigo
+ * @see A anotação @Scope serve para mudar o escopo do Controller 
+ * e o atributo proxyMode serve para o Spring criar um proxy envolvendo o objeto alvo (TARGET_CLASS) 
+ * afim de possibilitar que as informações do objeto da classe alvo possam ser transitadas de um escopo para outro.
+ * Assim será possível fazer com que o objeto da classe CarrinhoCompras seja acessível dentro dos Controllers sem ter 
+ * que mudar o escopo dos outros Controllers que precisam utilizar o objeto carrinho de compras. 
+ */
 @Component
 @Scope(value=WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class CarrinhoCompras implements Serializable {
@@ -59,6 +68,10 @@ public class CarrinhoCompras implements Serializable {
 		
 		itens.remove(new CarrinhoItem(produto,tipoPreco));
 		
+	}
+	
+	public void removerTodosItens() {
+		itens.clear();
 	}
 	
 	
