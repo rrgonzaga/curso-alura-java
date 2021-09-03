8770540b17d80346e4cb412831dc529b574ca105
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2021-09-03 09:45:56 -0300

    Commit Inicial - curso Java e JDBC - trabalhando com BD
    - Criação de uma fábrica de conexão;
    - Teste de recuperação de lista de registros;
    - Teste de inserção de registros.

---
+++ loja-virtual-repository/src/ConnectionFactory.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public Connection recuperarConexao() throws SQLException {
+		
+		Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+		
+		return connection;		
+	}
+	
+}
---
+++ loja-virtual-repository/src/TestaListagem.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+	
+	
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();		
+		Connection connection = connectionFactory.recuperarConexao();		
+		
+		Statement stm = connection.createStatement();
+		stm.execute("SELECT id, nome, descricao FROM PRODUTO");
+		
+		ResultSet rs = stm.getResultSet();
+		
+		while(rs.next()) {
+			Integer id = rs.getInt("id");
+			String nome = rs.getString("nome");
+			String descricao = rs.getString("descricao");
+			
+			System.out.println("id: " + id + ", nome: " + nome + ", descricao: " + descricao);
+		}	
+		
+		
+		
+		System.out.println("Fechando conexão!");
+		connection.close();
+	}
+
+}
