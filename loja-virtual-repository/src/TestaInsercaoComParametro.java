d98878e8a9e0ecacca6fcab67ee70abe25f804f3
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2021-11-19 09:00:24 -0300

    Aula 04 - Controle de transação
    - Uso da cláusula try-with-resources (O recurso em questão deve usar a interface Autoclosable);
    - Definição o controle da transação de inserção de forma não automática;
    - Fazendo a execução do método commit ou rollback de forma não automática, ou seja, explicitamente.

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -7,15 +7,44 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 	
 	public static void main(String[] args) throws SQLException {
-		String nome = "TECLADO BLUETOOTH LOGITECH''''";
-		String descricao = "TECLADO BLUETOOTH LOGITECH 2.5); delete from Produto;";
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.recuperarConexao();
+		/* Definindo o commit de cada transação de forma não automática. 
+		 * Para o nosso sistema ter o controle da transação e não o driver do JDBC.
+		*/ 
+		connection.setAutoCommit(false);
 		
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+		PreparedStatement stm = null;
 		
+		try {
+			stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			adicionarVariavel("TECLADO BLUETOOTH LOGITECH", "TECLADO BLUETOOTH LOGITECH 2.5", stm);
+			adicionarVariavel("Smart TV TCL ", "Smart TV TCL 40 polegadas Full HD", stm);
+			
+			connection.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			connection.rollback();
+			System.out.println("ROLLBACK EXECUTADO");
+		//O finally poderia ser substituido pelo try-with-resources.Assim não precisaria fechar o prepareStatement e nem a connection explicitamente
+		} finally {
+			if(stm != null && !stm.isClosed()) 
+				stm.close();
+			
+			if(!connection.isClosed()) 
+				connection.close();							
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
-		stm.setString(2, descricao);		
+		stm.setString(2, descricao);
+		
+//		if(nome.toLowerCase().startsWith("smart")) {
+//			throw new RuntimeException("Não foi possível adicionar o produto");
+//		}
 		
 		stm.execute();
 		
--- loja-virtual-repository/src/TestaRemocao.java
+++ loja-virtual-repository/src/TestaRemocao.java
@@ -3,18 +3,20 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TestaRemocao {
-	
+
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
-		
-		PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
-		stm.setInt(1, 2);
-		stm.execute();
-		
-		Integer linhasModificadas = stm.getUpdateCount();
-		
-		System.out.println("Quantidade de linhas que foram modificadas: " + linhasModificadas);		
-	}
 
+		//Uso do try-with-resources
+		try (Connection connection = factory.recuperarConexao();
+				PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?"))
+		{
+			stm.setInt(1, 2);
+			stm.execute();
+
+			Integer linhasModificadas = stm.getUpdateCount();
+
+			System.out.println("Quantidade de linhas que foram modificadas: " + linhasModificadas);
+		}
+	}
 }
