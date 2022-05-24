7f2b27d6dd8b52817c6d35191cae35a0622754b6
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2022-05-23 23:02:51 -0300

    Refatoração do método concederReajuste da classe ReajusteService

--- JUnit/src/main/java/br/com/alura/tdd/modelo/Desempenho.java
+++ JUnit/src/main/java/br/com/alura/tdd/modelo/Desempenho.java
@@ -1,7 +1,18 @@
 package br.com.alura.tdd.modelo;
 
-public enum Desempenho {
-    A_DESEJAR,
-    BOM, OTIMO;
+import java.math.BigDecimal;
 
+public enum Desempenho {
+    A_DESEJAR(new BigDecimal("0.03")),
+    BOM(new BigDecimal("0.15")),
+    OTIMO (new BigDecimal("0.20"));
+
+    private BigDecimal percentualReajuste;
+
+    Desempenho(BigDecimal percentualReajuste) {
+        this.percentualReajuste = percentualReajuste;
+    }
+    public BigDecimal getPercentualReajuste() {
+        return this.percentualReajuste;
+    }
 }
--- JUnit/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ JUnit/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,23 +8,7 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-
-        BigDecimal valorReajuste;
-
-        switch (desempenho) {
-            case A_DESEJAR:
-                valorReajuste = funcionario.getSalario().multiply(new BigDecimal(0.03));
-                break;
-            case BOM:
-                valorReajuste = funcionario.getSalario().multiply(new BigDecimal(0.15));
-                break;
-            case OTIMO:
-                valorReajuste = funcionario.getSalario().multiply(new BigDecimal(0.20));
-                break;
-            default:
-                valorReajuste = BigDecimal.ZERO;
-        }
-
+        BigDecimal valorReajuste = funcionario.getSalario().multiply(desempenho.getPercentualReajuste());
         funcionario.reajustarSalario(valorReajuste);
     }
 }
