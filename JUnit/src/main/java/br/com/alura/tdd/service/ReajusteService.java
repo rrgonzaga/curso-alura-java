089855eaa7d74ea4c350acffa8e122ac7282add1
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2022-05-30 20:04:29 -0300

    Alteração da assinatura do método concederReajuste

--- JUnit/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ JUnit/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -2,12 +2,13 @@ package br.com.alura.tdd.service;
 
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
+import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
 
 public class ReajusteService {
 
-    public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+    public void concederReajuste(@NotNull Funcionario funcionario, @NotNull Desempenho desempenho) {
         BigDecimal valorReajuste = funcionario.getSalario().multiply(desempenho.getPercentualReajuste());
         funcionario.reajustarSalario(valorReajuste);
     }
