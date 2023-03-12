a7ceeece6475ae213c01ab438b1ea3a80bd2c187
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2023-03-12 13:20:53 -0300

    commit inicial do projeto Design Patterns 01

---
+++ DesignPatterns01/src/br/com/alura/loja/imposto/CalculadoraImpostos.java
@@ -0,0 +1,22 @@
+package br.com.alura.loja.imposto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraImpostos {
+
+    public BigDecimal calcular(Orcamento orcamento, TipoImposto tipoImposto) {
+
+        switch (tipoImposto) {
+            case ICMS:
+                return orcamento.getValor().multiply(new BigDecimal("0.1"));
+            case ISS:
+                return orcamento.getValor().multiply(new BigDecimal("0.06"));
+            default:
+                return BigDecimal.ZERO;
+        }
+
+    }
+
+}
