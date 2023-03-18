8748fffc561b4dc79c68483664a7605a92d77204
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2023-03-18 13:27:21 -0300

    Implementação do design pattern Strategy.

--- DesignPatterns01/src/br/com/alura/loja/TestesImpostos.java
+++ DesignPatterns01/src/br/com/alura/loja/TestesImpostos.java
@@ -1,18 +1,16 @@
 package br.com.alura.loja;
 
 import br.com.alura.loja.imposto.CalculadoraImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.ICMS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
 public class TestesImpostos {
-
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraImpostos calculadoraImpostos = new CalculadoraImpostos();
 
-        System.out.println(calculadoraImpostos.calcular(orcamento, TipoImposto.ISS));
+        System.out.println(calculadoraImpostos.calcularImposto(orcamento, new ICMS()));
     }
-
 }
--- DesignPatterns01/src/br/com/alura/loja/imposto/CalculadoraImpostos.java
+++ DesignPatterns01/src/br/com/alura/loja/imposto/CalculadoraImpostos.java
@@ -5,18 +5,7 @@ import br.com.alura.loja.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public class CalculadoraImpostos {
-
-    public BigDecimal calcular(Orcamento orcamento, TipoImposto tipoImposto) {
-
-        switch (tipoImposto) {
-            case ICMS:
-                return orcamento.getValor().multiply(new BigDecimal("0.1"));
-            case ISS:
-                return orcamento.getValor().multiply(new BigDecimal("0.06"));
-            default:
-                return BigDecimal.ZERO;
-        }
-
+    public BigDecimal calcularImposto(Orcamento orcamento, Imposto imposto) {
+        return imposto.calcular(orcamento);
     }
-
 }
