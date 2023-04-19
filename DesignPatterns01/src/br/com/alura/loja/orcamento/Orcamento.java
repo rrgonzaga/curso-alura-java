591de9966bc381c1635eebc3992e04ab0ebf1b52
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2023-04-18 22:53:37 -0300

    Alteração da classe orçamento acrescentando uma nova propriedade chamada situacao e acrescentando também um novo método para aplicar um desconto extra

--- DesignPatterns01/src/br/com/alura/loja/orcamento/Orcamento.java
+++ DesignPatterns01/src/br/com/alura/loja/orcamento/Orcamento.java
@@ -3,8 +3,9 @@ package br.com.alura.loja.orcamento;
 import java.math.BigDecimal;
 
 public class Orcamento {
-    private final BigDecimal valor;
-    private final Integer quantidadeItens;
+    private BigDecimal valor;
+    private Integer quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, Integer quantidadeItens) {
         this.valor = valor;
@@ -16,5 +17,23 @@ public class Orcamento {
 
     public Integer getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    public String getSituacao() {
+        return this.situacao;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDescExtra = BigDecimal.ZERO;
+        if (situacao.equals("EM_ANALISE")) {
+            valorDescExtra = new BigDecimal("0.05");
+        } else if (situacao.equals("APROVADO")) {
+            valorDescExtra = new BigDecimal("0.02");
+        }
+
+        this.valor = this.valor.subtract(this.valor.multiply(valorDescExtra));
+    }
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 }
